8d2da81a7876b3f960a36c9073db6697c8b853a4
Yusuke Kageyama <kageyama_yusuke@aratana.jp>
2019-07-12 22:28:40 +0900

    シェル実行を関数化

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -2,5 +2,10 @@
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
+function execute() {
+    echo "[${SCRIPT_DIR}/${1}.sh] START"
+    /bin/bash "${SCRIPT_DIR}/${1}.sh"
+}
+
 # exec
-/bin/bash "${SCRIPT_DIR}/brew_init.sh"
+execute brew_init
