90a2ba86b6fa30a3c84be44036e69db30c6f6b42
Yusuke Kageyama <kageyama_yusuke@aratana.jp>
2019-07-16 23:09:36 +0900

    brew bundleに移行

---
+++ Brewfile
@@ -0,0 +1,50 @@
+# Brewfile
+cask_args appdir: "/Applications"
+tap "caskroom/cask"
+tap "homebrew/bundle"
+tap "homebrew/core"
+
+brew "anyenv"
+brew "openssl"
+brew "colordiff"
+brew "coreutils"
+brew "cowsay"
+brew "curl"
+brew "diff-so-fancy"
+brew "direnv"
+brew "gibo"
+brew "git"
+brew "git-secrets"
+brew "hub"
+brew "jq"
+brew "libzip"
+brew "lua"
+brew "peco"
+brew "protobuf"
+brew "sl"
+brew "ssh-copy-id"
+brew "thefuck"
+brew "tree"
+brew "vim"
+brew "wget"
+
+cask "atom"
+cask "cyberduck"
+cask "docker-toolbox"
+cask "funter"
+cask "google-japanese-ime"
+cask "vagrant"
+cask "vagrant-manager"
+cask "virtualbox"
+cask "xquartz"
+
+mas "Numbers", id: 409203825
+mas "Monity", id: 915542151
+mas "GarageBand", id: 682658836
+mas "iMovie", id: 408981434
+mas "Xcode", id: 497799835
+mas "Display Menu", id: 549083868
+mas "LINE", id: 539883307
+mas "Trello", id: 1278508951
+mas "Magnet", id: 441258766
+mas "TweetDeck", id: 485812721
--- bin/brew_init.sh
+++ bin/brew_init.sh
@@ -20,71 +20,8 @@ which brew >/dev/null 2>&1 && brew update
 echo_info "run brew upgrade..."
 brew upgrade
 
-formulas=(
-    git
-    wget
-    curl
-    tree
-    openssl
-    colordiff
-    cask
-    peco
-    hub
-    lua
-    "vim --with-lua"
-    ssh-copy-id
-    diff-so-fancy
-    thefuck
-    cowsay
-    anyenv
-    direnv
-    gibo
-    jq
-    protobuf
-    sl
-)
-
-echo_info "start brew install apps..."
-for formula in "${formulas[@]}"; do
-    if type $formula > /dev/null 2>&1; then
-        echo_warning "[${formula}] already exists."
-    else
-        brew install $formula || brew upgrade $formula
-        echo_info "[${formula}] installed."
-    fi
-done
-
-casks=(
-    firefox
-    google-chrome
-    google-japanese-ime
-    google-drive
-    slack
-    funter
-    refresh-finder
-    1password
-    iterm2
-    cyberduck
-    intellij-idea
-    atom
-    visual-studio-code
-    sourcetree
-    docker-toolbox
-    virtualbox
-    vagrant
-    vagrant-manager
-)
-
-echo_info "start brew cask install apps..."
-for cask in "${casks[@]}"; do
-    if type $formula > /dev/null 2>&1; then
-        echo_warning "[${cask}] already exists."
-    else
-        brew cask install $cask
-        echo_info "[${cask}] installed."
-    fi
-
-done
+echo_info "run brew bundle..."
+brew bundle
 
 echo_info "run brew cleanup..."
 brew cleanup -s
