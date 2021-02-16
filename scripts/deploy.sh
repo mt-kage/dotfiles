926c13415183d8081a59988521ef3f79e75151ce
mt-kage <mail@mt-kage.dev>
2021-02-16 13:13:27 +0900

    add vim deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,22 +1,31 @@
 #!/bin/bash
 
-readonly DOTFILES_DIR="$(dirname $(cd $(dirname $0); pwd))/configs"
+readonly DOTFILES_DIR="$(dirname $(cd $(dirname $0); pwd))"
+readonly CONFIGS_DIR="$DOTFILES_DIR/configs"
+readonly VIM_DIR="$DOTFILES_DIR/vim"
 
-function deploy_settings() {
-  echo "deploy settings start..."
-  cd ${DOTFILES_DIR} || exit 1
+function deploy_configs() {
+  echo "deploy configs start..."
+  cd ${CONFIGS_DIR} || exit 1
   for file in .??*; do
       [[ "$file" = ".git" ]] && continue
       [[ "$file" = ".DS_Store" ]] && continue
-      ln -fvns "$DOTFILES_DIR/$file" "$HOME/$file"
+      ln -fvns "$CONFIGS_DIR/$file" "$HOME/$file"
   done
 
-  echo "deploy settings success!"
+  echo "deploy configs success!"
+}
+
+function deploy_vim() {
+  echo "deploy vim start..."
+  ln -fvns ${VIM_DIR} "$HOME/.vim"
+  echo "deploy vim success!"
 }
 
 function deploy() {
   echo "deploy start."
-  deploy_settings
+  deploy_configs
+  deploy_vim
 
   source ~/.bashrc
   echo "deploy success!"
